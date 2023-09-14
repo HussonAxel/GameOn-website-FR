903441ba1da850c9716881a54e0fef6c0d21edab
HUSSON Axel <aaaxel.hsson@gmail.com>
2023-09-14 13:43:58 +0200

    fix some issues on form & add a form validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -31,4 +31,74 @@ document.addEventListener("click", function(event) {
   }
 });
 
+// form Validation
+const validateForm = function () {
+  const form = document.getElementById("form");
+  const nameErrorMessage = "Veuillez entrer 2 caractères ou plus."
+  const emailErrorMessage = "Veuillez entrer une adresse email valide."
+  const quantityErrorMessage = "Veuillez entrer un nombre entier positif."
 
+  const validateInput = function (inputElement, regex, errorMessage, fieldName) {
+    const inputValue = inputElement.value.trim();
+    const isValid = regex.test(inputValue);
+    const errorMessageElement = inputElement.nextElementSibling;
+    if (isValid) {
+      errorMessageElement.innerHTML = "";
+      inputElement.classList.add("success");
+      errorMessageElement.classList.remove("error-message");
+      inputElement.classList.remove("has-error");
+      return true;
+    } else {
+      errorMessageElement.innerHTML = `${errorMessage}`;
+      errorMessageElement.classList.add("error-message");
+      inputElement.classList.remove("success");
+      inputElement.classList.add("has-error");
+      return false;
+    }
+  };
+
+  form.first.addEventListener("change", function () {
+    validateInput(this, /^[a-zA-Z\- ]{2,}$/, nameErrorMessage, "Prénom");
+  });
+
+  form.last.addEventListener("change", function () {
+    validateInput(this, /^[a-zA-Z\- ]{2,}$/, nameErrorMessage, "Nom");
+  });
+
+  form.email.addEventListener("change", function () {
+    validateInput(this, /^[^@]+@[^@]+\.[^@]+$/, emailErrorMessage, "Email");
+  });
+
+  form.quantity.addEventListener("change", function () {
+    validateInput(this, /^[1-9]\d*$/, quantityErrorMessage, "Quantité");
+  });
+
+  form.addEventListener("submit", function (event) {
+    const locationInputs = document.querySelectorAll('input[name="location"]');
+    const termsCheckbox = document.getElementById("checkbox1");
+
+    let locationSelected = false;
+    for (let i = 0; i < locationInputs.length; i++) {
+      if (locationInputs[i].checked) {
+        locationSelected = true;
+        break;
+      }
+    }
+
+    if (!locationSelected) {
+      event.preventDefault();
+      const errorMessageElement = document.querySelector('.city-checkbox-error');
+      errorMessageElement.innerHTML = "Vous devez choisir une option.";
+      errorMessageElement.classList.add("error-message");
+    }
+
+    if (!termsCheckbox.checked) {
+      event.preventDefault();
+      const errorMessageElement = document.querySelector('.cgu-checkbox-error');
+      errorMessageElement.innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions.";
+      errorMessageElement.classList.add("error-message");
+    }
+  });
+};
+
+validateForm();
